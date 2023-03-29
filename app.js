659307b65374ea0e99dc36acfb00da5a4186f96f
Suz033 <susanamy2324@gmail.com>
2023-03-29 16:43:37 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,24 @@
 const express = require('express')
 const app = express()
 
+// dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI)
+
+// DB
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('=== mongodb error ===')
+})
+db.once('open', () => {
+  console.log('=== mongodb connected ===')
+})
+
+
 // route
 app.get('/', (req, res) => {
   res.send('start')
