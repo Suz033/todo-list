bc56f4d9ce84f059b14b44991807984e7aa1a6ad
Suz033 <susanamy2324@gmail.com>
2023-04-07 17:04:49 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
 //// modules ////
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const Todo = require('./models/todo')
 const methodOverride = require('method-override')
+
+// files
 const routes = require('./routes')
-
-
-// dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-mongoose.connect(process.env.MONGODB_URI)
-
-
-// DB
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('=== mongodb error ===')
-})
-db.once('open', () => {
-  console.log('=== mongodb connected ===')
-})
-
+require('./config/mongoose')
 
 // hbs
 app.engine('hbs', exphbs({ default: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-
 // body-parser
 app.use(express.urlencoded({ extended: true }))
-
 
 // method-override
 app.use(methodOverride('_method'))
@@ -41,7 +22,6 @@ app.use(methodOverride('_method'))
 //// routes ////
 // index
 app.use('/', routes)
-  
 
 // listen port
 const port = 3000
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,20 +1,10 @@
-const mongoose = require('mongoose')
+// files
 const Todo = require('../todo')
+const db = require('../../config/mongoose')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('=== mongodb error ===')
-})
 db.once('open', () => {
-  console.log('=== mongodb connected ===')
   for (let i = 0; i < 10; i++) {
     Todo.create({ name: `name-${i}` })
   }
-  console.log('script seed done.')
+  console.log('Seed script added.')
 })
