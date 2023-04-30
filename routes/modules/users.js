2a7a5e49ecd1b6f6af9913ef5020d1fd10967748
Suz033 <susanamy2324@gmail.com>
2023-04-30 19:17:49 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,10 @@
 // modules
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 // routers
+// login
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -10,8 +12,35 @@ router.get('/login', (req, res) => {
 router.post('/login', (req, res) => {
 })
 
+// register
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('User already exists.')
+        res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        return User.create({
+          name,
+          email,
+          password
+        })
+          .then(() => res.redirect('/'))
+          .catch(err => console.error(err))
+      }
+    })
+      .catch(err => console.log(err))
 })
 
 // exports
